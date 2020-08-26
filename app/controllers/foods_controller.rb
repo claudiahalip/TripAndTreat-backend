0f107018d34dfe5f1c0bda458927dc3b1efa253c
claudiahalip <claudia4ro@yahoo.com>
2020-08-26 12:42:00 -0500

    added create action for food and trip, create strong params for trip and food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -10,4 +10,18 @@ class FoodsController < ApplicationController
         render json: food.to_json(include: {trip: {only: [:location, :country, :date, :image]}}, only:[:name, :price, :rating, :description] )
     end 
 
+    def create
+       food = Food.new(food_params)
+       if food.save
+        render json: foods.to_json(include: {trip: {only: [:location, :country, :date, :image]}}, only:[:name, :price, :rating, :description])
+       else
+        render json: {error: "Your food couldn't be added"}
+    end 
+
+    private
+
+    def food_params
+        params.require(:food).permit(:name, :price, :rating, :description)
+    end 
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,11 +2,25 @@ class TripsController < ApplicationController
 
     def index 
         trips = Trip.all 
-        render json: trips.to_json(include: {food: {only: [:name, :price, :rating, :description]}}, only: [:location, :country, :date, :image])
+        render json: trips.to_json(include: {food: {only: [:id, :name, :price, :rating, :description]}}, only: [:id, :location, :country, :date, :image])
     end 
 
     def show 
         trip = Trip.find(params[:id])
-        render json: trip.to_json(include: {food: {only: [:name, :price, :rating, :description]}}, only: [:location, :country, :date, :image])
+        render json: trip.to_json(include: {food: {only: [:id, :name, :price, :rating, :description]}}, only: [:id, :location, :country, :date, :image])
+    end 
+
+    def creare
+        trip.Trip.new(trip_params)
+        if trip.save
+            render json: trips.to_json(include: {food: {only: [:id, :name, :price, :rating, :description]}}, only: [:id, :location, :country, :date, :image])
+        else 
+            render json: {error: "Your trip couldn't be added!"}
+    end
+
+    private 
+
+    def trip_params
+        params.require(:trip).permit(:location, :country, :date, :image) 
     end 
 end
